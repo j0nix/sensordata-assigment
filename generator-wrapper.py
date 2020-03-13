d7d2eed9b4d51487eb665bbc884c9bdbb961c3d6
Jon Svendsen <jon.svendsen@comhem.com>
2020-03-13 01:32:00 +0100

    almoast there

--- generator-wrapper.py
+++ generator-wrapper.py
@@ -1,27 +1,115 @@
-import subprocess, socket, os, time
-'''
-generator wrapper. Send sensor data output with UDP over port 10514
-'''
-# Define UDP sender
-HOST, PORT = "127.0.0.1", 10514
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-# open a subprocess with generator
-file = os.path.dirname(os.path.realpath(__file__)) + "/bin/sensor_data.x86_64-unknown-linux-gnu"
-process = subprocess.Popen([file,'-n j0nix'], stdout=subprocess.PIPE,bufsize=0,shell=True)
-print("Running subprocess on PID {}".format(process.pid))
-# while generator running, send output over UDP
-while True:
-    # get generator output
-    data = process.stdout.read(100)
-    # send generator output
-    sock.sendto(data, (HOST, PORT))
-    print("sent data")
-    # check if process is finished
-    return_code = process.poll()
-    if return_code is not None:
-        ## untested code, the idea is to parse whatever way be left after process is finished
-        print('RETURN CODE', return_code)
-        # Process has finished, read rest of the output
-        for output in process.stdout.read(100):
-    	    sock.sendto(data, (HOST, PORT))
-        break
+import sys, subprocess, socket, os, time, logging, string, random
+from argparse import ArgumentParser
+from argparse import RawTextHelpFormatter
+
+""" Generator wrapper. Send sensor data output with UDP over port 10514 """
+
+__author__ = "Jon Svendsen"
+__version__ = "1.0"
+epilog = """
+
+Example)
+   python {} -s 3
+______________________________________
+
+"""
+
+## Argument parser setup
+parser = ArgumentParser(
+    formatter_class=RawTextHelpFormatter,
+    description=__doc__,
+    epilog=epilog,
+    prog=os.path.basename(sys.argv[0]),
+)
+
+parser._action_groups.pop()
+optional = parser.add_argument_group(title="optional arguments")
+optional.add_argument(
+    "-v", "--verbose", action="count", default=0, help="increase log level"
+)
+optional.add_argument(
+    "-q", "--quiet", action="count", default=0, help="decrease log level"
+)
+required = parser.add_argument_group(title="required arguments")
+required.add_argument(
+    "-s",
+    "--spawn",
+    dest="spawn",
+    metavar='N',
+    type=int,
+    required=True,
+    help="how many processes to spawn",
+)
+args = parser.parse_args()
+
+"""Logging setup"""
+log_adjust = max(min(args.quiet - args.verbose, 2), -2)
+logging.basicConfig(
+    level=logging.INFO + log_adjust * 10,
+    format="%(levelname)-8s[%(module)10s] %(message)s",
+)
+
+# Simple string generator
+def randomString(stringLength=16):
+    """Generate a random string of fixed length """
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(stringLength))
+
+def processSpawner(name,command):
+
+    # Define UDP sender
+    host, port = "127.0.0.1", 10514
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # define 
+    procs = []
+    # start subprocesses
+    #for sub in command:
+    #    procs.append(subprocess.Popen(sub,stdout=subprocess.PIPE,bufsize=0,shell=True))
+    #    time.sleep(1)
+    procs = [ subprocess.Popen(i,stdout=subprocess.PIPE,bufsize=0,shell=True) for i in command ]
+    logging.info("all processes started")
+
+    # while generator running, send output over UDP
+    while True:
+        for p in procs:
+            # get generator output
+            data = None
+            try:
+                data = p.stdout.read(1024)
+            except:
+                pass
+
+            # send generator output
+            if data:
+                start=0
+                while start < len(data):
+                    p_size = int.from_bytes(data[start:start+4], "big")
+                    sock.sendto(data[start:start+p_size], (host, port))
+                    logging.debug("'Sensor:{} with PID {}' sent {} to {}:{}".format(name[procs.index(p)],p.pid,data[start:start+p_size],host,port))
+                    start+=p_size
+
+                # check if process is finished
+                return_code = p.poll()
+                if return_code is not None:
+                    ## untested code, the idea is to parse whatever way be left after process is finished
+                    logging.debug('RETURN CODE', return_code)
+                    # Process has finished, read rest of the output
+                    for output in p.stdout.read(34):
+                        sock.sendto(data.encode("utf-8"), (host, port))
+                    break
+            else:
+                logging.debug("{} - {}".format(p.pid,error))
+        #p.wait()
+
+if __name__ == "__main__":
+
+    logging.info("Spawning {} generator processes".format(args.spawn))
+    sensor_name = []
+    command= []
+
+    for x in range(args.spawn):
+        sensor_name.append(randomString())
+        command.append(os.path.dirname(os.path.realpath(__file__)) + "/bin/sensor_data.x86_64-unknown-linux-gnu --name {}".format(sensor_name[x]))
+        logging.info("starting sensor {}".format(sensor_name[x]))
+    else:
+        processSpawner(sensor_name,command)
--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-import sys, socket, time, pytz, json
+import sys, socket, time, pytz, json, socket, threading
 from struct import *
 from datetime import datetime
 
@@ -20,163 +20,218 @@ Offset      Field name      Type            Size        Description
 --------------------------------------------------------------------------------------------------
 - Both temperature and humidity are optional.
 - This means that the offset for humidity can also be 13 + nlen
-'''
-
 """
 
-def decodeSensorData(message, msg_size, timestamp, name_length):
 
-    # Set start and stop position for name in packet
-    # we then use n_stop as start position for parsing temperature and humidity
-    n_start, n_stop = (13, 13 + name_length)
+def incoming_message(data, address):
 
-    # Get name from packet
-    name = unpack(">{}s".format(name_length), message[n_start:n_stop])
+    """ Handle the client """
+    # handle request
 
-    # Parse epoch timestamp and set timestamp to ISO8601 with time zone
-    ISO_timestamp = "{}{}:00".format(
-        datetime.fromtimestamp(timestamp / 1000).isoformat()[:-3],
-        datetime.now(pytz.timezone("Europe/Stockholm")).strftime("%z")[0:3],
-    )
 
-    # Prepare dictonary we will return for read sensor data
-    # if below condition evaluates as false we return dict as is
-    json_dict = {
-        "timestamp": ISO_timestamp,
-        "name": name[0].decode("utf-8"),
-        "temperature": None,
-        "humidity": None,
-    }
+""" A simple UDP Server """
 
-    """
-    We have 4 usecases to consider
-    1. n_stop == msg_size. That means there is nothing left to parse and we can skip further actions
-    3. msg_size - n_stop = 2. We have humidity data to parse, but no temerature data
-    2. msg_size - n_stop = 3. We have temparature data to parse, but no humidity data
-    4. msg_size - n_stop = 5. We have both temparature and humidity data to parse
-    """
 
-    # Usecase 1, Check if we need to parse sensor data
-    if msg_size > n_stop:
+class UDPServer:
+    def __init__(self, host, port):
+        self.host = host  # Host address
+        self.port = port  # Host port
+        self.sock = None  # Socket
+        # create socket and bind server to the address + port
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.bind((self.host, self.port))
 
-        # Usecase 2, only humidity?
-        if msg_size - n_stop == 2:
-            json_dict["humidity"] = unpack(">H", message[n_stop:msg_size])[0]
+    def recv_message(self):
+        # Listen for incoming messages
+        try:
+            data, address = self.sock.recvfrom(1024)
+        except Exception as e:
+            print("Failed to fetch incoming message from socket ({})".format(e))
 
-        # Usecase 3, only temperature?
-        if msg_size - n_stop == 3:
-            json_dict["temperature"] = int.from_bytes(message[n_stop:msg_size], "big")
-            """ Alternative solution for parsing 3 bytes with struct => Make it 4 bytes!
-                tmp = bytearray()
-                tmp.extend(b'\x00')
-                tmp.extend(message[n_stop:msg_size])
-                temperature = unpack(">I", tmp)
-            """
+        print(data.decode('utf-8'))
 
-        # Usecase 4, both temperature and humidity ?
-        if msg_size - n_stop == 5:
-            json_dict["temperature"] = int.from_bytes(
-                message[n_stop : msg_size - 2], "big"
+    def shutdown(self):
+        self.sock.close()
+
+
+""" A simple UDP Server for handling multiple clients """
+
+
+class UDPSensorPacketParser(UDPServer):
+    def __init__(self, host, port):
+        super().__init__(host, port)
+        self.socket_lock = threading.Lock()
+
+    def incoming_message(self, data, address):
+        # UDP Packet Headers
+        p_size, p_timestamp, p_nlength = (None, None, None)
+        try:
+            # Parse header data. size & nlength (name length) will be used to eveluate and parse the rest of the message
+            p_size, p_timestamp, p_nlength = unpack(">IQB", data[:13])
+
+            # make sure that the numbers adds up
+            if len(data) == p_size:
+                # Decode rest of message and log as json to a logfile
+                parsed_dict = self.decode_sensor_data(
+                    data, p_size, p_timestamp, p_nlength
+                )
+                try:
+                    self.log_data(parsed_dict)
+                except Exception as e:
+                    print("failed to send data to log outout ({})".format(e))
+            else:
+                print("Exception: data:{}, len(data):{}, packet_size_in_header:{})".format(data,len(data),p_size))
+                raise error
+        except error as e:
+            print("failed to parse packet({}) from {} ({})".format(data, address, e))
+
+    def recv_message(self):
+        """ Wait for clients and handle their requests """
+        try:
+            while True:  # keep alive
+
+                try:  # receive request from client
+                    data, address = self.sock.recvfrom(1024)
+
+                    c_thread = threading.Thread(
+                        target=self.incoming_message, args=(data, address)
+                    )
+                    c_thread.daemon = True
+                    c_thread.start()
+
+                except OSError as e:
+                    print(e)
+
+        except KeyboardInterrupt:
+            self.shutdown()
+
+    def log_data(self, data):
+        try:
+            jsonData = json.dumps(data, indent=4)
+            print(jsonData)
+        except Exception as e:
+            print(e)
+            raise
+
+    def decode_sensor_data(self, message, msg_size, timestamp, name_length):
+
+        # Set start and stop position for name in packet
+        # we then use n_stop as start position for parsing temperature and humidity
+        n_start, n_stop = (13, 13 + name_length)
+
+        # Dictonary to save parsed data into ... or not, depending on what comes our way
+        json_dict = {
+            "timestamp": None,
+            "name": None,
+            "temperature": None,
+            "humidity": None,
+        }
+
+        # Get name from packet
+        try:
+            name = unpack(">{}s".format(name_length), message[n_start:n_stop])
+        except error as e:
+            print(
+                "failed to unpack name ({}), start pos: {}, stop_pos: {}, message: {}, ignoring packet".format(
+                    e, n_start, n_stop, message
+                )
             )
-            json_dict["humidity"] = int.from_bytes(
-                message[n_stop + 3 : msg_size], "big"
+            return json_dict
+
+        # Parse epoch timestamp and set timestamp to ISO8601 with time zone
+        try:
+            ISO_timestamp = "{}{}:00".format(
+                datetime.fromtimestamp(timestamp / 1000).isoformat()[:-3],
+                datetime.now(pytz.timezone("Europe/Stockholm")).strftime("%z")[0:3],
             )
-    else:
+        except ValueError as e:
+            print("failed to parse date data ({}), ignore packet".format(e))
+            return json_dict
+
+        # Add data to dictonary we will return from read sensor data
+        json_dict = {
+            "timestamp": ISO_timestamp,
+            "name": name[0].decode("utf-8"),
+            "temperature": None,
+            "humidity": None,
+        }
+
+        """
+        We have 4 usecases to consider
+        1. n_stop == msg_size. That means there is nothing left to parse and we can skip further actions
+        2. msg_size - n_stop = 2. We have humidity data to parse, but no temerature data
+        3. msg_size - n_stop = 3. We have temparature data to parse, but no humidity data
+        4. msg_size - n_stop = 5. We have both temparature and humidity data to parse
+        """
+
+        # Usecase 1, Check if we need to parse sensor data
+        if msg_size > n_stop:
+
+            # Usecase 2, only humidity?
+            if msg_size - n_stop == 2:
+                json_dict["humidity"] = unpack(">H", message[n_stop:msg_size])[0]
+
+            # Usecase 3, only temperature?
+            if msg_size - n_stop == 3:
+                json_dict["temperature"] = int.from_bytes(
+                    message[n_stop:msg_size], "big"
+                )
+                """ Alternative solution for parsing 3 bytes with struct => Make it 4 bytes!
+                    tmp = bytearray()
+                    tmp.extend(b'\x00')
+                    tmp.extend(message[n_stop:msg_size])
+                    temperature = unpack(">I", tmp)
+                """
+
+            # Usecase 4, both temperature and humidity ?
+            if msg_size - n_stop == 5:
+                json_dict["temperature"] = int.from_bytes(
+                    message[n_stop : msg_size - 2], "big"
+                )
+                json_dict["humidity"] = int.from_bytes(
+                    message[n_stop + 3 : msg_size], "big"
+                )
+        else:
+            return json_dict
+
+        """
+        I assume the following when proceed
+        - Temperature are in hundreds of K that I translate that the three first digits is in Kelvin
+          meaning than no measure values are lower than 100 K. All digits after the three first is decimals
+        - Humidity is measured between 1 to 100. Numbers greater than 100, third digit is a decimal
+        """
+        # Do we have temp value?
+        if json_dict["temperature"]:
+            try:
+                # Take first three digits and make rest decimal, round to one decimal
+                json_dict["temperature"] = round(
+                    float(str(json_dict["temperature"])[:3])
+                    + float("0." + str(json_dict["temperature"])[4:]),
+                    1,
+                )
+            except Exception as e:
+                print("Error when modeling temperature with decimals")
+
+        # Do we have humidity value?
+        # Do we need to add decimal to value?
+        if json_dict["humidity"] and json_dict["humidity"] > 100:
+            try:
+                # Take two first digits and make rest decimals, round to one decimal
+                json_dict["humidity"] = round(
+                    float(str(json_dict["humidity"])[:2])
+                    + float("0." + str(json_dict["humidity"])[3:]),
+                    1,
+                )
+            except Exception as e:
+                print("Error when modeling humidity with decimals")
+
         return json_dict
 
-    """
-    I assume the following when proceed
-    - Temperature are in hundreds of K that I translate that the three first digits is in Kelvin
-      meaning than no measure values are lower than 100 K. All digits after the three first is decimals
-    - Humidity is measured between 1 to 100. Numbers greater than 100, third digit is a decimal
-    """
-    # Do we have temp value?
-    if json_dict["temperature"]:
-        try:
-            # Take first three digits and make rest decimal, round to one decimal
-            json_dict["temperature"] = round(
-                float(str(json_dict["temperature"])[:3])
-                + float("0." + str(json_dict["temperature"])[4:]),
-                1,
-            )
-        except Exception as e:
-            print("Error when modeling temperature with decimals")
 
-    # Do we have humidity value?
-    # Do we need to add decimal to value?
-    if json_dict["humidity"] and json_dict["humidity"] > 100:
-        try:
-            # Take two first digits and make rest decimals, round to one decimal
-            json_dict["humidity"] = round(
-                float(str(json_dict["humidity"])[:2])
-                + float("0." + str(json_dict["humidity"])[3:]),
-                1,
-            )
-        except Exception as e:
-            print("Error when modeling humidity with decimals")
-
-    return json_dict
-
-
-def logData(data):
-    try:
-        jsonData = json.dumps(data, indent=4)
-        print(jsonData)
-    except Exception as e:
-        print(e)
-        raise
-
+def main():
+    # Create UDP server and listen for incoming packets from generator-wrapper.py
+    server = UDPSensorPacketParser("127.0.0.1", 10514)
+    server.recv_message()
 
 if __name__ == "__main__":
-
-    """
-    Create UDP listener for incoming packets from generator-wrapper.py
-    generatior-wrapper simulates incoming UDP traffic to this packet parser & log script.
-    """
-
-    try:
-        # Server options
-        local_ip, local_port, buffer_size = "127.0.0.1", 10514, 1024
-        # Create a datagram socket
-        UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-        # Bind to address and ip & port
-        UDPServerSocket.bind((local_ip, local_port))
-    except Exception as e:
-        print("Failed starting UDP server, {}".format(e))
-        sys.exit(1)
-    else:
-        print(
-            "Server up and listening for incoming UDP on {}:{}".format(
-                local_ip, local_port
-            )
-        )
-
-    # Init variables to make them global
-    # These represent header data for incoming UDP packets
-    p_size, p_timestamp, p_nlength = (None, None, None)
-    # Listen for incoming datagrams
-    while True:
-        try:
-            # Fetch incoming message
-            # Have not read up on buffer size and how I should think to make this optimal.
-            # Now only picked a value that works...
-            incoming_msg = UDPServerSocket.recvfrom(buffer_size)
-        except Exception as e:
-            print("Failed to fetch data from UDP socket")
-        else:
-            # set message data and adress to dedicated variables, to clearify things
-            message = incoming_msg[0]
-            address = incoming_msg[1]
-
-            try:
-                # Parse header data. size & nlength (name length) will be used to eveluate and parse the rest of the message
-                p_size, p_timestamp, p_nlength = unpack(">IQB", message[:13])
-            except error as e:
-                print("failed to parse packet(): {}".format(message, e))
-            else:
-                # Decode rest of message and log as json to a logfile
-                parsed_dict = decodeSensorData(message, p_size, p_timestamp, p_nlength)
-                try:
-                    logData(parsed_dict)
-                except Exception as e:
-                    print("failed to send data to log outout")
+    main()
